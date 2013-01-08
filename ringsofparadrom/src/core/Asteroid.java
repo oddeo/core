b3ee2f86e4c43bd15f70f6a1ff2ac8a9ba5b1417
da101025 <daniel.p.jones@gmail.com>
2013-01-07 22:59:47 -0700

    Added test member to asteroid again

--- ringsofparadrom/src/core/Asteroid.java
+++ ringsofparadrom/src/core/Asteroid.java
@@ -2,7 +2,7 @@ package core;
 
 public class Asteroid implements Entity {
   private String name;
-  private String test;
+  private String test2;
   
   public Asteroid(String name) {
     this.name = name;
