72280bd6af09b863450dd236c370b245731b4b26
da101025 <daniel.p.jones@gmail.com>
2013-01-13 08:47:43 -0700

    Created Galaxy Map that contains a hashmap of nodes.  Class contains a method to initialize the generation of the galaxy, random connections between nodes, random placement of entities, and random placement of ships

---
+++ ringsofparadrom/src/core/Galaxy.java
@@ -0,0 +1,22 @@
+package core;
+
+import java.util.*;
+
+/**
+ *
+ * @author danieljones
+ */
+public class Galaxy {
+    private Map points;
+    
+    public Galaxy() {
+        this.points = new HashMap();
+    }
+    
+    public void initialize(List nodes, List ships) {
+        // generate random connections for nodes
+        // place asteroids in random nodes
+        // place planets in random nodes
+        // place ship in random node
+    }
+}
