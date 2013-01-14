43a1839b6d2191bac5ee264a2401f5540c407193
da101025 <daniel.p.jones@gmail.com>
2013-01-13 23:45:53 -0700

    Added JUnit test project and unit tests

---
+++ ringsofparadrom/test/core/GalaxyTest.java
@@ -0,0 +1,39 @@
+package core;
+
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author danieljones
+ */
+public class GalaxyTest {
+
+    /**
+     * Test of putPoint method, of class Galaxy.
+     */
+    @Test
+    public void testPutPoint() {
+        System.out.println("putPoint");
+        Object point = new Node("point0");
+        Galaxy instance = new Galaxy();
+        instance.putPoint(point);
+        assertSame(instance.getPoint("point0"), point);
+    }
+
+
+    /**
+     * Test of toString method, of class Galaxy.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Galaxy instance = new Galaxy();
+        instance.putPoint(new Node("n0"));
+        instance.putPoint(new Node("n1"));
+        String expResult = "{n1:n1,n0:n0}";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+}
